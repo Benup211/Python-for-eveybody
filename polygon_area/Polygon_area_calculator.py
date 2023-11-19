c513e1d1b811d084dc9c4f2122aa516a36140d80
benup211 <benup211@gmail.com>
2023-11-19 21:00:41 +0545

    finish:Polygon Area Calculator

--- polygon_area/Polygon_area_calculator.py
+++ polygon_area/Polygon_area_calculator.py
@@ -16,33 +16,24 @@ class Rectangle:
     def get_diagonal(self):
         return ((self._width ** 2 + self._height ** 2) ** .5)
     def get_picture(self):
-        if (self._width*self._height)>50:
+        if (self._width>50 or self._height>50):
             return "Too big for picture."
         else:
             return (("*"*self._width+"\n")*self._height)
     def get_amount_inside(self,shape):
-        print(shape)
+        return int(self.get_area()/shape.get_area())
     #str
     def __str__(self):
         return f"Rectangle(width={self._width}, height={self._height})"
 class Square(Rectangle):
     _side=0
     def __init__(self,side):
-        super().__init__(side,side)
-        self._side=side
+        self._width=side
+        self._height=side
     #setter
     def set_side(self,side):
-        self._side=side
         self._width=side
         self._height=side
-    def set_width(self,side):
-        self._width=side
-        self._height=side
-        self._side=side
-    def set_height(self,side):
-        self._width=side
-        self._height=side
-        self._side=side
     #str
     def __str__(self):
-        return f"Square(side={self._side})"
+        return f"Square(side={self._width})"
